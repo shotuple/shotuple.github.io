86ab01a891002b2cae6c8e9f072658212c430df9
shotuple <shotuple@gmail.com>
2022-10-06 17:27:59 +0900

    Baek

--- baekjoon/_posts/test.py
+++ baekjoon/_posts/test.py
@@ -1,1 +1,12 @@
-pip3 install bs4
+# A = map(int,list(input().split()))
+# A = list(map(int, input().split()))
+# print(next(A))
+# print(next(A))
+# print(next(A))
+
+n,x=map(int,input().split())
+#a=map(int,list(input().split()))
+a = list(map(int, input().split()))
+for i in a:
+    if i<x:
+        print(i,end=" ")
